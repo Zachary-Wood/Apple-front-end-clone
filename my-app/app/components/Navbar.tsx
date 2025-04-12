8ea1eebf57aae41edb124694b31e479e54582973
Zach Wood <zacharydavwood@gmail.com>
2025-04-11 20:42:48 -0400

    finished navbar mock up

--- my-app/app/components/Navbar.tsx
+++ my-app/app/components/Navbar.tsx
@@ -3,9 +3,9 @@ import { FaApple, FaSearch } from "react-icons/fa";
 
 export const Navbar = () => {
   return (
-    <div className="fixed top-0 w-full bg-[rgba(0,0,0,0.8)] z-50 h-[60px]">
+    <div className="fixed top-0 w-full bg-[rgba(0,0,0,0.8)] z-50">
 
-      <div className="flex flex-row justify-between items-center gap-4 sm:gap-6 h-15 md:gap-8 lg:gap-8 mx-auto cursor-pointer text-xs w-3/5 p-4">
+      <div className="flex flex-row justify-between items-center gap-4 sm:gap-6 md:gap-8 lg:gap-8 mx-auto cursor-pointer text-xs w-3/5 p-4">
         <FaApple className="text-lg text-white" />
         <p>Store</p>
         <p>Mac</p>
