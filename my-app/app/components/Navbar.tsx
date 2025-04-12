bf0f7bc2e59f62690f879230122726e6ad2b152f
Zach Wood <zacharydavwood@gmail.com>
2025-04-11 20:44:55 -0400

    fixed up nabar

--- my-app/app/components/Navbar.tsx
+++ my-app/app/components/Navbar.tsx
@@ -4,9 +4,8 @@ import { FaApple, FaSearch } from "react-icons/fa";
 export const Navbar = () => {
   return (
     <div className="fixed top-0 w-full bg-[rgba(0,0,0,0.8)] z-50">
-
-      <div className="flex flex-row justify-between items-center gap-4 sm:gap-6 md:gap-8 lg:gap-8 mx-auto cursor-pointer text-xs w-3/5 p-4">
-        <FaApple className="text-lg text-white" />
+      <div className="flex flex-row flex-wrap justify-between items-center gap-4 sm:gap-6 md:gap-8 lg:gap-8 mx-auto cursor-pointer text-xs max-w-screen-xl p-4 text-white overflow-x-auto">
+        <FaApple className="text-lg flex-shrink-0" />
         <p>Store</p>
         <p>Mac</p>
         <p>iPad</p>
@@ -18,10 +17,11 @@ export const Navbar = () => {
         <p>Entertainment</p>
         <p>Accessories</p>
         <p>Support</p>
-        <FaSearch className="text-lg text-white" />
-        <IoBagOutline className="text-lg text-white" />
+        <FaSearch className="text-lg flex-shrink-0" />
+        <IoBagOutline className="text-lg flex-shrink-0" />
       </div>
     </div>
   );
 };
+
 
