3ad5fde1c6d04a54fb642f72758bddbc8ad74d05
Zach Wood <zacharydavwood@gmail.com>
2025-04-14 14:02:52 -0400

    added next cnfig file

--- my-app/next.config.ts
+++ my-app/next.config.ts
@@ -1,7 +1,14 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+
+  output: 'export',
+  
+  
+  distDir: 'out',
+
+ 
+  reactStrictMode: true,
 };
 
 export default nextConfig;
